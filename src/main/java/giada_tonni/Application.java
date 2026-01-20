f8b5ece579a4f1605505099dcfeec19c4ad98f99
Giada <tonni.giada.210802@gmail.com>
2026-01-20 14:31:39 +0100

    Initial commit

---
+++ src/main/java/giada_tonni/Application.java
@@ -0,0 +1,13 @@
+package giada_tonni;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Application {
+
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("consegnas15l2pu");
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
